d0b432bb1137f7547df6e3b238969795cb60d547
Roshan9623 <dhangarroshan321@gmail.com>
2026-01-04 12:01:21 +0530

    taking input from user and made multiplication and show output

--- mult.c
+++ mult.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 int main(){
-    int a=12;
-    int b=1;
+    int a;
+    printf("Enter first number: ");
+    scanf("%d",&a);
+    int b;
+    printf("Enter the second number: ");
+    scanf("%d",&b);
     int c=a*b;
-    printf("%d",c);
+    printf("Result is: %d",c);
     return 0;
 }
