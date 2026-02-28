964d8c6035371b21802a0a1bfc7fd7c618c19a01
Roshan9623 <dhangarroshan321@gmail.com>
2026-03-01 05:15:03 +0530

    added  new do-while.c

---
+++ 03_Loops/do-while.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+    int main(){
+
+      int n;
+      printf(" entera number : ");
+      scanf("%d",&n);
+      int i=1;
+      do{
+        printf("hello vs code \n");
+        i++;
+        
+      }
+       while(i<=n);
+
+        return 0;
+    }
