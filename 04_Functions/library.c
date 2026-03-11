9cfe66305077e6d5e6408961c0f77f1c82afaa99
Roshan9623 <dhangarroshan321@gmail.com>
2026-03-11 08:15:41 +0530

    Added library.c

---
+++ 04_Functions/library.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int a;
+    printf("Enter a squar : ");
+    scanf("%d",& a);
+
+    int root=sqrt(a);
+
+    printf("The squar root is :%d",root);
+
+
+ return 0;
+
+}
